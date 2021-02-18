55b63bdd161a04a9847444ca67157c2856ec0c33
Jeff Carley <jeff.carley@gmail.com>
2021-02-18 09:59:53 -0600

    sidemenu updates

--- client/src/features/dashboard/Dashboard.tsx
+++ client/src/features/dashboard/Dashboard.tsx
@@ -10,17 +10,28 @@ export const Dashboard = (): ReactElement => {
   let { url } = useRouteMatch();
 
   return (
-    <div className='grid-container'>
-      <div className='sidebar border-r-2 shadow-xl'>
-        <SideNavBar />
+    <div className='dashboard-grid-container'>
+      <div className='dashboard-toolbar'>
+        <span className='inline-block h-8 text-2xl font-semibold align-bottom'>CarleyFamily.com</span>
+        <span className='cursor-pointer text-blue-500 hover:text-blue-800 transition-colors'>
+          <label htmlFor='check'>
+            <i className='fas fa-bars'></i>
+          </label>
+        </span>
       </div>
-      <div className='content'>
-        <Switch>
-          <Route path={`${url}/photos`} component={Photos} />
-          <Route path={`${url}/sharing`} component={Sharing} />
-          <Route path={`${url}/albums`} component={Albums} />
-          <Route path={`${url}/trash`} component={Trash} />
-        </Switch>
+      <div className='dashboard-main'>
+        <input type='checkbox' name='check' id='check' />
+        <div className='sidebar'>
+          <SideNavBar />
+        </div>
+        <div className='debug content'>
+          <Switch>
+            <Route path={`${url}/photos`} component={Photos} />
+            <Route path={`${url}/sharing`} component={Sharing} />
+            <Route path={`${url}/albums`} component={Albums} />
+            <Route path={`${url}/trash`} component={Trash} />
+          </Switch>
+        </div>
       </div>
     </div>
   );
